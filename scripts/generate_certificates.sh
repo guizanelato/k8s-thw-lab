0ad4bb2a7725e7dcc39739c4c1bef9fb0cc0106e
Guilherme <guilherme.zanelato@4linux.com.br>
2021-03-04 18:18:16 -0300

    add: added scripts to generate certificates
    Added scripts to generate:
    - kube-controller certificate
    - kube-proxy
    - kube-scheduler
    - kube-api
    - service-accounts

--- scripts/generate_certificates.sh
+++ scripts/generate_certificates.sh
@@ -43,11 +43,10 @@ for worker in worker-01 worker-02 worker-03; do
    -new \
    -nodes \
    -sha256 \
-   -subj "/C=BR/ST/L=None/O=system:nodes/CN=$worker,172.20.12.$ip" \
+   -subj "/C=BR/ST/L=None/O=system:nodes/CN=kube-node" \
    -key certs/$worker.key \
    -out certs/$worker.csr;
 
-   ip=$(echo "$ip+10" | bc);
 done
   	
 
@@ -55,11 +54,134 @@ done
 for worker in worker-01 worker-02 worker-03; do
   openssl x509 -req \
     -days 365 \
+	-extfile <(printf "subjectAltName=$worker,172.20.10.$ip") \
     -sha256 \
     -CA certs/kubernetes-ca.crt \
     -CAkey certs/kubernetes-ca.key \
     -set_serial 01 \
     -extensions req_ext \
     -in certs/$worker.csr \
-    -out certs/$worker.crt 
+    -out certs/$worker.crt
+
+    ip=$(echo "$ip+10" | bc);
+
 done
+
+
+#4.1: generate kube-controller-manager private key
+openssl	genrsa -out certs/kube-controller-manager.key 4096
+
+#4.2: generate certificate sign request
+openssl req \
+  -new \
+  -nodes \
+  -sha256 \
+  -subj "/C=BR/ST=None/L=None/O=None/CN=system:kube-controller-manager" \
+  -key certs/kube-controller-manager.key \
+  -out certs/kube-controller-manager.csr 
+
+#4.3: sign kube-controller-certificate
+openssl x509 \
+  -req \
+  -days 365 \
+  -sha256 \
+  -CA certs/kubernetes-ca.crt \
+  -CAkey certs/kubernetes-ca.key \
+  -set_serial 01 \
+  -extensions req_ext \
+  -in certs/kube-controller-manager.csr \
+  -out certs/kube-controler-manager.crt
+
+#5.1: generate kube proxy private key
+openssl genrsa -out certs/kube-proxy.key 4096
+
+#5.2: generate certificate sign request
+openssl req \
+  -new \
+  -nodes \
+  -sha256 \
+  -subj "/C=BR/ST=None/L=None/O=None/CN=system:kube-proxy" \
+  -key certs/kube-proxy.key \
+  -out certs/kube-proxy.csr
+
+#5.3: sign kube-proxy certificate
+openssl x509 \
+  -req \
+  -days 365 \
+  -sha256 \
+  -CA certs/kubernetes-ca.crt \
+  -CAkey certs/kubernetes-ca.key \
+  -set_serial 01 \
+  -extensions req_ext \
+  -in certs/kube-proxy.csr \
+  -out certs/kube-proxy.crt
+
+#6.1: generate kube-scheduler private key
+openssl genrsa -out certs/kube-scheduler.key 4096
+
+#6.2: generate sign certificate request
+openssl req \
+  -new \
+  -nodes \
+  -sha256 \
+  -subj "/C=BR/ST=None/L=None/O=None/CN=system:kube-scheduler" \
+  -key certs/kube-scheduler.key \
+  -out certs/kube-scheduler.csr
+
+#6.3 sign kube-scheduler certificate
+openssl x509 \
+ -req \
+ -days 365 \
+ -sha256 \
+ -CA certs/kubernetes-ca.crt \
+ -CAkey certs/kubernetes-ca.key \
+ -set_serial 01 \
+ -extensions req_ext \
+ -in certs/kube-scheduler.csr \
+ -out certs/kube-scheduler.crt
+
+
+echo kubeapi-server
+#7.1 generate kube-apiserver private key
+openssl genrsa -out certs/kube-apiserver.key 4096
+
+#7.2 generate certificate sign request
+openssl req \
+  -new \
+  -nodes \
+  -sha256 \
+  -subj "/C=BR/ST=None/L=None/O=None/CN=system:kube-apiserver" \
+  -key certs/kube-apiserver.key \
+  -out certs/kube-apiserver.csr
+
+
+#7.3 sign kube-apiserver certificate
+openssl x509 -req -days 365  -extfile <(printf "subjectAltName=IP:10.1.0.1,IP:127.0.0.1,DNS:kubernetes.default.svc.cluster.local,DNS:kube-apiserver,DNS:localhost") -sha256 -CA certs/kubernetes-ca.crt -CAkey certs/kubernetes-ca.key -set_serial 01 -in certs/kube-apiserver.csr -out certs/kube-apiserver.crt
+
+
+
+#8.1: generate service account private key
+openssl genrsa -out certs/service-account.key 4096
+
+#8.2: generate certificate sign request
+openssl req \
+  -new \
+  -nodes \
+  -sha256 \
+  -subj "/C=BR/ST=None/L=None/O=None/CN=service-accounts" \
+  -key certs/service-account.key \
+  -out certs/service-account.csr
+
+#8.3 sign service account certificate
+openssl x509 \
+  -req \
+  -days 365 \
+  -CA certs/kubernetes-ca.crt \
+  -CAkey certs/kubernetes-ca.key \
+  -set_serial 01 \
+  -extensions req_ext \
+  -in certs/service-account.csr \
+  -out certs/service-account.crt
+  
+
+
