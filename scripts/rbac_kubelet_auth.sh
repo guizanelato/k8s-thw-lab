d8ee74075b04b2e7c84d9a14928c235640c834ad
Guilherme <guilherme.zanelato@4linux.com.br>
2021-03-13 20:01:29 -0300

    add: added rbac for kubelet authorization

---
+++ scripts/rbac_kubelet_auth.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+#Warning: running these on one manager is enough
+
+
+#1: Create ClusterRole to enable Kubelet API access
+
+cat <<EOF | kubectl apply --kubeconfig admin.kubeconfig -f -
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRole
+metadata:
+  annotations:
+    rbac.authorization.kubernetes.io/autoupdate: "true"
+  labels:
+    kubernetes.io/bootstrapping: rbac-defaults
+  name: system:kube-apiserver-to-kubelet
+rules:
+  - apiGroups:
+      - ""
+    resources:
+      - nodes/proxy
+      - nodes/stats
+      - nodes/log
+      - nodes/spec
+      - nodes/metrics
+    verbs:
+      - "*"
+EOF
+
+
+#2 Bind the system:kube-apiserver-to-kubelet ClusterRole to the kubernetes user:
+
+
+cat <<EOF | kubectl apply --kubeconfig admin.kubeconfig -f -
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRoleBinding
+metadata:
+  name: system:kube-apiserver
+  namespace: ""
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: system:kube-apiserver-to-kubelet
+subjects:
+  - apiGroup: rbac.authorization.k8s.io
+    kind: User
+    name: kubernetes
+EOF
+
